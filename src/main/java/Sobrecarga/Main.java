360bb53757e20af2a8231fff76771b607545d097
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-25 16:41:25 -0300

    Sobrecarga

---
+++ src/main/java/Sobrecarga/Main.java
@@ -0,0 +1,12 @@
+package Sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,6d);
+        Quadrilatero.area(9,8,4.5);
+        Quadrilatero.area(10f,10.5f);
+    }
+}
