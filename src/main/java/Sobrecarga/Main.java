a160a407c06366f8d29cffc02fdcc290fb14d257
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-11-25 16:52:28 -0300

    Retornos

--- src/main/java/Sobrecarga/Main.java
+++ src/main/java/Sobrecarga/Main.java
@@ -4,9 +4,25 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
+       /*  Quadrilatero.area(3);
         Quadrilatero.area(5d,6d);
         Quadrilatero.area(9,8,4.5);
-        Quadrilatero.area(10f,10.5f);
+        Quadrilatero.area(10f,10.5f);*/
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5d,10d);
+        System.out.println("Área do retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(5,3,4);
+        System.out.println("Área do quadrado: " + areaTrapezio);
+
+
+
+
+
+
+
     }
 }
